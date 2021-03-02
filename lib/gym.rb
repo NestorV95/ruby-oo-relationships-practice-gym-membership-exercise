ba01fac5261dd5a37d00ecca5e8c8526c1d8bf83
Nestor <nestor.venegas95@gmail.com>
2021-03-02 14:48:07 -0600

    commit tree 5f2a455538788c75f23692d0459b2729dc8f95cf

--- lib/gym.rb
+++ lib/gym.rb
@@ -12,19 +12,19 @@ class Gym
      @@all
   end
 
-  def specific_memberships(gym) #Get a list of all memberships at a specific gym
-
+  def specific_memberships #Get a list of all memberships at a specific gym
+     Membership.all.select{|membership| membership.gym == self}
   end
 
-  def members_belong_to( gym) #Get a list of all the lifters that have a membership to a specific gym
-
+  def members_belong_to #Get a list of all the lifters that have a membership to a specific gym
+     self.specific_memberships.map{|membership| membership.lifter}
   end
 
   def lifter_name #Get a list of the names of all lifters that have a membership to that gym
-
+     members_belong_to.map{|lifter| lifter.name}
   end
 
   def total_num_memberships #Get the combined lift total of every lifter has a membership to that gym
-
+     members_belong_to.map{|lifter| lifter.total_lift}.sum
   end
 end
--- lib/lifter.rb
+++ lib/lifter.rb
@@ -15,22 +15,24 @@ class Lifter
 
   def lifter_memberships #Get a list of all the memberships that a specific lifter has
      # Lifter.all.select{|object| object.name == self} returns lifter matching name
-     
+     Membership.all.select{|membership| membership.lifter == self}
   end
 
   def lifter_gyms  #Get a list of all the gyms that a specific lifter has memberships to
-
+      lifter_memberships.map{|membership| membership.gym}
   end
 
-  def lifter_average #Get the average lift total of all lifters
-
+  def self.lifter_average #Get the average lift total of all lifters
+     total_lift = 0
+     self.all.each{|lifter| total_lift += lifter.total_lift}
+     total_lift / self.all.count
   end
 
   def total_cost #Get the total cost of a specific lifter's gym memberships
-
+     memberships.map{|membership| membership.cost}.sum
   end
 
-  def  find_new_gym(gym, membership) #Given a gym and a membership cost, sign a specific lifter up for a new gym
-
+  def  find_new_gym(gym, cost) #Given a gym and a membership cost, sign a specific lifter up for a new gym
+      Membership.new(cost, gym, self)
   end
 end
